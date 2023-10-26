99aeebfe7492f6d7e7c1b7e078d781bf6e6d1ccf
mnik7044 <mnikwork7802@gmail.com>
2023-10-27 00:55:05 +0530

    initialise migration from mongodb to sqlite prisma

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import { Router } from "express";
-import Post from "../models/posts.js";
+// import Post from "../models/posts.js";
 
 const router = Router();
+const prisma = new PrismaClient()
 
 router.get("/", async (req, res) => {
   try {
-    const posts = await Post.find();
-    res.json(posts);
+    const savedPost = await prisma.Post.findMany();
+    console.log(savedPost)
+    res.json({});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while fetching all posts" });
@@ -15,33 +18,34 @@ router.get("/", async (req, res) => {
 
 router.post("/create", async (req, res) => {
   try {
-    const { content, cover, author } = req.body;
-    const newPost = new Post({
-      content,
-      cover,
-      author,
-    });
-    const savedPost = await newPost.save();
-    res.json(savedPost);
+    await prisma.Post.create(
+      {
+        data: req.body
+      }
+    )
+    res.status(200).json({ response: true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Failed to create a new post" });
+    res.status(500).json({
+      response: false,
+      error: "Failed to create a new post"
+    });
   }
 });
 
-router.delete("/delete/:postId", async (req, res) => {
-  try {
-    const postId = req.params.postId;
-    const post = await Post.findById(postId);
-    if (!post) {
-      return res.status(404).json({ error: "Post not found" });
-    }
-    await post.remove();
-    res.json({ message: "Post deleted successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "An error occurred while deleting the post" });
-  }
-});
+// router.delete("/delete/:postId", async (req, res) => {
+//   try {
+//     const postId = req.params.postId;
+//     const post = await Post.findById(postId);
+//     if (!post) {
+//       return res.status(404).json({ error: "Post not found" });
+//     }
+//     await post.remove();
+//     res.json({ message: "Post deleted successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "An error occurred while deleting the post" });
+//   }
+// });
 
 export default router;
