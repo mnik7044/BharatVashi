2b6ae529893af2a1d1a25fda782c5075f45da41d
Ekam-Bitt <ekambitt@gmail.com>
2023-10-28 12:21:48 +0530

    bharatvasi backend

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,33 +1,47 @@
 import { Router } from "express";
-import Comment from "../models/comment.js";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
-
-router.get("/", async (req, res) => {
-  try {
-    const posts = await Comment.find();
-    res.json(posts);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "An error occurred while fetching all comments" });
-  }
-});
+const primsa = new PrismaClient();
 
 router.post("/create", async (req, res) => {
+  const { postId } = req.body;
+  // console.log(req.body);
   try {
-    const { content, author, postId } = req.body;
-    const newComment = new Comment({
-      content,
-      author,
-      postId,
+    const post = await primsa.Post.findMany({
+      where: {
+        id: postId,
+      },
     });
-    await newComment.save();
-    res.json({ message: "Comment added successfully", comment: newComment });
+    if (!post) {
+      res.json(400).json({ response: false, error: "Post Not Found" });
+      return;
+    }
+    const cmt = await primsa.Comment.create({
+      data: res.body,
+    });
+    res.json({ response: true, data: cmt });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "An error occurred while adding the comment" });
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching all comments" });
   }
 });
+
+// router.post("/create", async (req, res) => {
+//   try {
+//     const data = req.body;
+//     const newComment = new Comment(data);
+//     await newComment.save();
+//     res.json({ message: "Comment added successfully", comment: newComment });
+//   } catch (error) {
+//     console.error(error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while adding the comment" });
+//   }
+// });
 
 router.delete("/delete/:commentId", async (req, res) => {
   try {
@@ -44,7 +58,9 @@ router.delete("/delete/:commentId", async (req, res) => {
     res.json({ message: "Comment deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "An error occurred while deleting the comment" });
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the comment" });
   }
 });
 
